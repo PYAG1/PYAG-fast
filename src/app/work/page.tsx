45e3c6ee914e67fc799fc9f3fe3b787ca0141ea9
Papa Yaw Agyeman-Gyekye. <96929032+PYAG1@users.noreply.github.com>
2024-07-05 13:49:41 +0000

    updates

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -28,7 +28,7 @@ const page = () => {
   ]
   return (
     <AppLayout>
-      <div className=" pt-10 flex flex-col gap-10" k>
+      <div className=" pt-10 flex flex-col gap-10" >
      {
       work.map((item,index)=>{
         return ( <div key={index} className="hover:border-l-4 border-l-primary2 hover:pl-2 duration-300 ">
